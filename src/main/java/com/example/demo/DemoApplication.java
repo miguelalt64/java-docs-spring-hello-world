76e1e4f785aedc0238367e95b494bb1c61c014b6
miguelalt64 <48498631+miguelalt64@users.noreply.github.com>
2022-08-28 01:53:47 -0500

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
 @RestController
