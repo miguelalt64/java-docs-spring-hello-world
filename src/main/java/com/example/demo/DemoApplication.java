2c8210fd048a26e13428e2da0bca3ac9dbb96e91
miguelalt64 <48498631+miguelalt64@users.noreply.github.com>
2022-08-28 01:41:22 -0500

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @SpringBootApplication
 @RestController
@@ -13,8 +15,9 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@RequestMapping("/")
+	@GetMapping("/profesores")
+	@ResponseStatus(HttpStatus.OK)
 	String sayHello() {
-		return "Hello World!";
+		return "Hello World interno, desde profesores!";
 	}
 }
